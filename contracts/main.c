e4a76ec63b97fc913e3fcc7a6b841838d9fb108a
Marc Catalán Sánchez <marc.prat.2001@gmail.com>
2024-12-06 12:46:59 +0100

    Cleaning main.c

--- contracts/main.c
+++ contracts/main.c
@@ -11,15 +11,13 @@
 uint8_t buf_out[32];
 
 // succeed and return a bebi32
-ArbResult inline _return_success_bebi32(bebi32 const retval)
-{
+ArbResult inline _return_success_bebi32(bebi32 const retval) {
   ArbResult res = {Success, retval, 32};
   return res;
 }
 
-bool inline is_valid_address(uint8_t *input, size_t len)
-{
-  // validate input is an address padded to 32 bytes
+// Validate if input is a valid address (padded to 32 bytes)
+bool inline is_valid_address(uint8_t *input, size_t len) {
   if (len != 32)
     return false;
 
@@ -29,68 +27,25 @@ bool inline is_valid_address(uint8_t *input, size_t len)
   return true;
 }
 
-ArbResult calldata_len(uint8_t *input, size_t len)
-{
+ArbResult calldata_len(uint8_t *input, size_t len) {
   bebi32_set_u32(buf_out, len);
   return _return_success_bebi32(buf_out);
 }
 
-char *ft_strnstr(const char *haystack, const char *needle, size_t len)
-{
-  int i;
-  size_t limit;
-
-  i = 0;
-  limit = 0;
-  if ((*needle == 0 && *haystack == 0) || (*needle == 0 && len == 0))
-    return ((char *)haystack);
-  while (*haystack && len && limit < len)
-  {
-    i = 0;
-    while (limit + i < len && needle[i] && needle[i] == haystack[i])
-      ++i;
-    if (needle[i] == 0)
-      return ((char *)haystack);
-    haystack++;
-    limit++;
-  }
-  return (NULL);
-}
-
-ArbResult ping_pong(uint8_t *input, size_t len)
-{
-  if (ft_strnstr(input, "ping", len))
-    return _return_short_string(Success, "pong");
-  return _return_short_string(Success, "ping");
-}
-
-ArbResult hola_mundo(uint8_t *input, size_t len)
-{
-	return _return_short_string(Success, "Hola Mundo");
-}
-
-ArbResult mi_balance(uint8_t *input, size_t len)
-{
+// Function to retrieve the caller's padded address
+ArbResult mi_balance(uint8_t *input, size_t len) {
 	msg_sender_padded(buf_out);
 	return _return_success_bebi32(buf_out);
 }
 
-int handler(size_t argc)
-{
-  // Save the function calldata (selector(bytes4) + fn_args(bytes))
+// Main handler to route calls to the appropriate function
+int handler(size_t argc) {
   uint8_t argv[argc];
-  read_args(argv); // 4 btes (firma del a funcion)
+  read_args(argv); // Read function selector and calldata
 
-  // Define the registry array with registered functions
+  // Registry of functions
   FunctionRegistry registry[] = {
-    // balance()
-    // address: 0x82B36e0c4C6E9cafA5CeACf481fa13e6CE2ac385
-    // uint256: 100000000000000000000....32bytes
-    // string: "242242121"
-    // bytes32: 0x82B36ac385
       {to_function_selector("calldata_len()"), calldata_len},
-      {to_function_selector("ping_pong(bytes32)"), ping_pong},
-      {to_function_selector("hola_mundo()"), hola_mundo},
       {to_function_selector("mi_balance()"), mi_balance},
   };
 
@@ -101,6 +56,7 @@ int handler(size_t argc)
                                 sizeof(registry) / sizeof(registry[0]),
                                 signature, argv + 4, argc - 4 // Exclude the selector from calldata
   );
+  // Write the result and return the status
   return (write_result(res.output, res.output_len), res.status);
 }
 
